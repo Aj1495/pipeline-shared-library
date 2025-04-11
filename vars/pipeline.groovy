7ab1cb38a9569fffb1b836a49d40aea7c0bb4cff
skswami91 <skswami91@gmail.com>
2025-04-11 23:11:09 +0530

    Update and rename firstGroovyFunction.groovy to pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,51 @@
+def call(String masterBuild) {
+  def git_app_repo = scm.userRemoteConfigs[0].url
+  def SERVICE_NAME = scm.getUserRemoteConfigs()[0].getUrl().tokenize('/').last().split("\\.")[0]
+  def git_app_branch = "${env.BRANCH_NAME}"
+
+  node() {
+    properties([
+      buildDiscarder(logRotator(numToKeepStr: '5')),
+      disableConcurrentBuilds(),
+    ])
+
+    stage('CleanWorkspace') {
+      cleanWs()
+      sh 'whoami && pwd'
+    }
+
+    stage('Checkout Code') {
+      checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${git_app_branch}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [
+          [$class: 'LocalBranch', localBranch: "${git_app_branch}"]
+        ],
+        submoduleCfg: [],
+        userRemoteConfigs: [
+          [credentialsId: '1360ab06-c1b5-4bc8-bc4d-89977f8400cf', url: "${git_app_repo}"]
+        ]
+      ])
+    }
+
+    /* 
+    * Call your shared library functions, each in its own stage
+    */
+
+    stage('Build Docker Image') {
+      dockerBuild(SERVICE_NAME, git_app_branch)
+    }
+
+    stage('Push Docker Image') {
+      dockerPush(SERVICE_NAME, git_app_branch)
+    }
+
+    stage('Helm Create Manifests') {
+      createHelmManifests(SERVICE_NAME, git_app_branch)
+    }
+    stage('Push Kubernetes Manifests') {
+      pushK8sManifests(SERVICE_NAME, git_app_branch)
+    }
+  }
+}
