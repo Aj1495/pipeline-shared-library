dfa3c6ab183c5b8b45b3418de9e34e6a06bd1720
skswami91 <skswami91@gmail.com>
2025-04-11 23:26:47 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,11 +1,33 @@
-def call(String serviceName) {
-  ansiColor('xterm') {
-    try {
-      echo "Building Docker image: ${serviceName}"
-      sh "docker build -t ${serviceName} ."
-    } catch (Exception e) {
-      currentBuild.result = 'FAILURE'
-      throw e
+def call(String serviceName, String branchName) {
+  stage('Docker Build') {
+    ansiColor('xterm') {
+      withCredentials([
+        usernamePassword(
+          credentialsId: 'dockerCreds',
+          usernameVariable: 'DOCKER_USER',
+          passwordVariable: 'DOCKER_PASS'
+        )
+      ]) {
+        try {
+          sh """
+            echo "Logging into Docker Registry..."
+            docker login -u \$DOCKER_USER -p \$DOCKER_PASS
+          """
+          if (branchName in ["staging", "preprod", "production", "master"]) {
+            echo "Building Docker image for production like environment ... "
+            sh "docker build -t ${serviceName} ."
+            // if there's an 'app' subdirectory or second Dockerfile
+            sh "cd app && docker build -t ${serviceName} ."
+          } else {
+            echo "Building Docker image for non-production environment... "
+            sh "docker build -t ${serviceName} ."
+          }
+        
+        } catch (Exception e) {
+          currentBuild.result = 'FAILURE'
+          throw e
+        }
+      }
     }
   }
 }
