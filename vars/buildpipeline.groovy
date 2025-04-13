0d4273e5b1933c9b63779935859e1669ef61ced5
skswami91 <skswami91@gmail.com>
2025-04-13 19:39:00 +0530

    Update buildpipeline.groovy

--- vars/buildpipeline.groovy
+++ vars/buildpipeline.groovy
@@ -33,4 +33,59 @@ def call(String masterBuild) {
       )
     ],
     volumes: [
-      emptyDirVolume(m
+      emptyDirVolume(mountPath: '/var/lib/docker', memory: false)
+    ]
+  ) {
+    node('jenkins-agent') {
+      properties([
+        buildDiscarder(logRotator(numToKeepStr: '5')),
+        disableConcurrentBuilds(),
+      ])
+
+      stage('CleanWorkspace') {
+        sh 'whoami && pwd'
+      }
+
+      stage('Checkout Code') {
+        checkout([
+          $class: 'GitSCM',
+          branches: [[name: "*/${git_app_branch}"]],
+          doGenerateSubmoduleConfigurations: false,
+          extensions: [
+            [$class: 'LocalBranch', localBranch: "${git_app_branch}"]
+          ],
+          submoduleCfg: [],
+          userRemoteConfigs: [
+            [credentialsId: '1360ab06-c1b5-4bc8-bc4d-89977f8400cf', url: "${git_app_repo}"]
+          ]
+        ])
+      }
+
+      /*
+      * Call your shared library functions, each in its own stage
+      */
+
+      stage('Build Docker Image') {
+        container('docker') {
+          dockerBuild(SERVICE_NAME, git_app_branch)
+        }
+      }
+
+      stage('Push Docker Image') {
+        container('docker') {
+          dockerPush(SERVICE_NAME, git_app_branch)
+        }
+      }
+
+      stage('Helm Create Manifests') {
+        container('helm') {
+          createHelmManifests(SERVICE_NAME, git_app_branch)
+        }
+      }
+
+      stage('Push Kubernetes Manifests') {
+        pushK8sManifests(SERVICE_NAME, git_app_branch)
+      }
+    }
+  }
+}
