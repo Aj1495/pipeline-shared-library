a74078da4284d6c41b561a1dc406c5928fed4afe
skswami91 <skswami91@gmail.com>
2025-04-13 22:45:49 +0530

    Update createHelmManifests.groovy

--- vars/createHelmManifests.groovy
+++ vars/createHelmManifests.groovy
@@ -9,6 +9,7 @@ def call(String serviceName, String branchName) {
           cd helm_charts
           helm create ${serviceName}
           sed -i "s|tag:.*|tag: \\"${env.BUILD_NUMBER}\\"|" ${serviceName}/values.yaml
+          sed -i "s|repository:.*|repository: \\"${env.BUILD_NUMBER}\\"|" ${serviceName}/values.yaml
           #Additional commands for templating or customizing values.yaml
         """
       } catch (Exception e) {
