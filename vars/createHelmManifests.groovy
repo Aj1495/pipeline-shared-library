831193c06c47499b51fbeff1e13c22925ad673d9
skswami91 <skswami91@gmail.com>
2025-04-13 18:42:55 +0000

    modifying helm values

--- vars/createHelmManifests.groovy
+++ vars/createHelmManifests.groovy
@@ -22,7 +22,7 @@ def call(String serviceName, String branchName) {
             helm create ${serviceName}
             sed -i "s|tag:.*|tag: \\"${env.BUILD_NUMBER}\\"|" ${serviceName}/values.yaml
             sed -i "s|repository:.*|repository: \\"${DOCKER_USER}/${serviceName}\\"|" ${serviceName}/values.yaml
-            sed -i 's|ingress:\\n  enabled: false\\n  className: ""|ingress:\\n  enabled: true|' ${serviceName}/values.yaml
+            sed -i '/^ingress:/,/^[^ ]/ s/enabled: false/enabled: true/' ${serviceName}/values.yaml
             sed -i 's|annotations: {}|annotations:\\n    kubernetes.io/ingress.class: "alb"\\n    alb.ingress.kubernetes.io/scheme: "internet-facing"\\n    alb.ingress.kubernetes.io/healthcheck-path: "/node-app"|' ${serviceName}/values.yaml
             sed -i 's|path: /|path: /node-app|' ${serviceName}/values.yaml
             sed -i 's|pathType: ImplementationSpecific|pathType: Prefix|' ${serviceName}/values.yaml
