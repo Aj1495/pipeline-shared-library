bb43db47e8eb61e4b765c1f1bed17a89ea39eb5f
skswami91 <skswami91@gmail.com>
2025-04-13 17:32:42 +0000

    resolving DOCKER_USER error

--- vars/createHelmManifests.groovy
+++ vars/createHelmManifests.groovy
@@ -1,21 +1,30 @@
 def call(String serviceName, String branchName) {
   stage('Create Helm Manifests') {
     ansiColor('xterm') {
-      try {
-        sh """ 
-          echo "Creating Helm charts for ${serviceName}..."
-          rm -rf helm_charts
-          mkdir helm_charts
-          cd helm_charts
-          helm create ${serviceName}
-          sed -i "s|tag:.*|tag: \\"${env.BUILD_NUMBER}\\"|" ${serviceName}/values.yaml
-          sed -i "s|repository:.*|repository: \\"${DOCKER_USER}/${serviceName}\\"|" ${serviceName}/values.yaml
-          #Additional commands for templating or customizing values.yaml
-        """
-      } catch (Exception e) {
-        currentBuild.result = 'FAILURE'
-        throw e
+      withCredentials([
+        usernamePassword(
+          credentialsId: 'dockerCreds',
+          usernameVariable: 'DOCKER_USER',
+          passwordVariable: 'DOCKER_PASS'
+        )
+      ]) {
+        try {
+          sh """ 
+            echo "Creating Helm charts for ${serviceName}..."
+            rm -rf helm_charts
+            mkdir helm_charts
+            cd helm_charts
+            helm create ${serviceName}
+            sed -i "s|tag:.*|tag: \\"${env.BUILD_NUMBER}\\"|" ${serviceName}/values.yaml
+            sed -i "s|repository:.*|repository: \\"${DOCKER_USER}/${serviceName}\\"|" ${serviceName}/values.yaml
+            #Additional commands for templating or customizing values.yaml
+          """
+        } catch (Exception e) {
+          currentBuild.result = 'FAILURE'
+          throw e
+        }
       }
     }
   }
 }
+      
