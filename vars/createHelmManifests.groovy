727a8ba63864d8fcdcf1ed3d6085cc0c6e17af4e
skswami91 <skswami91@gmail.com>
2025-04-13 18:10:10 +0000

    adding helm values

--- vars/createHelmManifests.groovy
+++ vars/createHelmManifests.groovy
@@ -6,6 +6,11 @@ def call(String serviceName, String branchName) {
           credentialsId: 'dockerCreds',
           usernameVariable: 'DOCKER_USER',
           passwordVariable: 'DOCKER_PASS'
+        ),
+        usernamePassword(
+          credentialsId: 'alb-dns-name',
+          usernameVariable: 'ALB',
+          passwordVariable: 'ALB_DNS'
         )
       ]) {
         try {
@@ -17,6 +22,7 @@ def call(String serviceName, String branchName) {
             helm create ${serviceName}
             sed -i "s|tag:.*|tag: \\"${env.BUILD_NUMBER}\\"|" ${serviceName}/values.yaml
             sed -i "s|repository:.*|repository: \\"${DOCKER_USER}/${serviceName}\\"|" ${serviceName}/values.yaml
+            sed -i 's/ingress:\\n  enabled: false\\n  className: ""\\n  annotations: {}/ingress:\\n  enabled: true\\n  annotations:\\n    kubernetes.io\\/ingress.class: "alb"\\n    alb.ingress.kubernetes.io\\/scheme: internet-facing\\n    alb.ingress.kubernetes.io\\/healthcheck-path: "node-app"/' ${serviceName}/values.yaml
             #Additional commands for templating or customizing values.yaml
           """
         } catch (Exception e) {
