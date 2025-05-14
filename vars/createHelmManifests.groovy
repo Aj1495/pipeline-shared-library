54734017d4b83132bc0d936be6e525534b1c7445
Aj1495 <akn.me006n3@gmail.com>
2025-05-14 17:51:48 +0530

    Update createHelmManifests.groovy

--- vars/createHelmManifests.groovy
+++ vars/createHelmManifests.groovy
@@ -26,7 +26,7 @@ def call(String serviceName, String branchName) {
             sed -i 's|annotations: {}|annotations:\\n    kubernetes.io/ingress.class: "alb"\\n    alb.ingress.kubernetes.io/scheme: "internet-facing"\\n    alb.ingress.kubernetes.io/healthcheck-path: "/node-app"|' ${serviceName}/values.yaml
             sed -i 's|path: /|path: /node-app|' ${serviceName}/values.yaml
             sed -i 's|pathType: ImplementationSpecific|pathType: Prefix|' ${serviceName}/values.yaml
-            sed -i 's|host: chart-example.local|host: "${ALB_DNS}"|' ${serviceName}/values.yaml
+            sed -i 's|host: chart-example.local|host: \"${ALB_DNS}\"|' ${serviceName}/values.yaml
             sed -i 's|port: 80|port: 3000|' ${serviceName}/values.yaml
             #Additional commands for templating or customizing values.yaml
           """
